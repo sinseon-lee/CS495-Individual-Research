52c06d2950fe296d1b91905df91b22b1bcfc1f47
Sinseon Lee <sinseonky@gmail.com>
2020-01-20 16:34:42 +0900

    add 'sender' file

---
+++ 2.ZED-SDK-Camera-Streaming/sender/src/main.cpp
@@ -0,0 +1,57 @@
+// ZED includes
+#include <sl/Camera.hpp>
+
+// Sample includes
+#include "utils.hpp"
+
+// Using namespace
+using namespace sl;
+
+int main(int argc, char **argv) {
+    // Create a ZED camera
+    Camera zed;
+
+    // Set configuration parameters for the ZED
+    InitParameters initParameters;
+    initParameters.camera_resolution = RESOLUTION_HD720;
+    initParameters.camera_fps = 60;
+    initParameters.depth_mode = DEPTH_MODE_NONE;
+    initParameters.sdk_verbose = true;
+
+    // Open the camera
+    ERROR_CODE err = zed.open(initParameters);
+    if (err != SUCCESS) {
+        std::cout << toString(err) << std::endl;
+        zed.close();
+        return -1; // Quit if an error occurred
+    }
+
+    sl::StreamingParameters stream_params;
+    stream_params.codec = sl::STREAMING_CODEC_HEVC;
+    stream_params.bitrate = 6000;
+    if(argc > 1) stream_params.port = atoi(argv[1]);
+
+    err = zed.enableStreaming(stream_params);
+    if (err != SUCCESS) {
+        std::cout << "Streaming initialization error. " << toString(err) << std::endl;
+        zed.close();
+        return -2;
+    }
+
+    std::cout << "Streaming on port " << stream_params.port << std::endl;
+
+    SetCtrlHandler();
+
+    int fc = 0;
+    while (!exit_app) {
+        if (zed.grab() == SUCCESS) {
+            sl::sleep_ms(1);
+            fc++;
+        }
+    }
+
+    // Stop recording
+    zed.disableStreaming();
+    zed.close();
+    return 0;
+}
